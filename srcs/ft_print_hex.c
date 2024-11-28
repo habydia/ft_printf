be00b2f90112486a6e86ab6d288ae734d69289e3
hadia <hadia@student.42lyon.fr>
2024-11-28 13:10:27 +0100

    add

--- srcs/ft_print_hex.c
+++ srcs/ft_print_hex.c
@@ -9,53 +9,50 @@
 /*   Updated: 2024/11/27 16:30:16 by hadia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
-int    ft_print_uphex(unsigned int n)
+int ft_print_uphex(unsigned int n)
 {
-	int	print_len;
+    int print_len = 0;
+    char c;
 
-	print_len = 0;
-        
-               if (n >= 16)
-                {
-                        print_len += ft_print_hex(n / 16);
-                }
-                else
-				write(1, &"0123456789ABCDEF", [n + 'a'], 1);
-		return print_len;
-
+    if (n >= 16)
+    {
+        print_len += ft_print_uphex(n / 16);
+    }
+    c = "0123456789ABCDEF"[n % 16];
+    write(1, &c, 1);
+    print_len++;
+    return (print_len);
 }
 
-int    ft_print_lowhex(unsigned int)
+int ft_print_lowhex(unsigned int n)
 {
-	int	print_len;
+    int print_len = 0;
+    char c;
 
-	print_len = 0;
-        
-               if (n >= 16)
-                {
-                        ft_print_hex(n / 16);
-                        write(1, &"0123456789abcdef"[n % 10], 1);
-                }
-                else
-				write(1, &"0123456789abcdef", [n + 'a'], 1);
-		return print_len;
-
+    if (n >= 16)
+    {
+        print_len += ft_print_lowhex(n / 16);
+    }
+    c = "0123456789abcdef"[n % 16];
+    write(1, &c, 1);
+    print_len++;
+    return (print_len);
 }
 
-int    ft_print_hex(unsigned int n, char format)
+int ft_print_hex(unsigned int n, char format)
 {
-	int	print_len;
+    int print_len = 0;
 
-	print_len = 0;
-
-	if (format == 'X')
-	{
-		print_len = ft_print_uphex(n);
-	}
-	else if (format == x)
-	{
-		ft_print_lowhex(n);
-	}
-	return (print_len;)
+    if (format == 'X')
+    {
+        print_len = ft_print_uphex(n);
+    }
+    else if (format == 'x')
+    {
+        print_len = ft_print_lowhex(n);
+    }
+    return (print_len);
 }
+
